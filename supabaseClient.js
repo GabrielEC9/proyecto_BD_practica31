fb71e7053917739d6eb9093896af25df163a9c87
GabrielEC9 <161523458+GabrielEC9@users.noreply.github.com>
2025-10-11 16:03:39 -0600

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lvuqrksujmgwgvebokgw.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = 'https://lvuqrksujmgwgvebokgw.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

7e84696b1c03c3f5bbeb15d33b51ef72d06f44f7
GabrielEC9 <161523458+GabrielEC9@users.noreply.github.com>
2025-10-11 16:04:27 -0600

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-
+import express from 'express';
 import { supabase } from './supabaseClient.js';
 
-async function main() {
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
   const { data, error } = await supabase.from('cliente').select('*');
+  if (error) return res.status(500).send(error.message);
+  res.json(data);
+});
 
-  if (error) {
-    console.error('Error al obtener datos:', error);
-  } else {
-    console.log('Datos obtenidos:', data);
-  }
-}
-
-main();
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});

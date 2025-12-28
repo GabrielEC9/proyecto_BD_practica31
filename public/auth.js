940ada28dbdb99b7f43f5608a5556803f0b266c3
GabrielEC9 <161523458+GabrielEC9@users.noreply.github.com>
2025-12-27 22:17:53 -0600

    Actualizacion ORM

--- public/auth.js
+++ public/auth.js
@@ -1,10 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
-import bcrypt from 'bcryptjs'
-
-const supabaseUrl = 'https://lvuqrksujmgwgvebokgw.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('login-form')
   const errorMsg = document.getElementById('error-message')
@@ -18,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     errorMsg.classList.add('hidden')
 
     try {
-      const res = await fetch('/api/login', { 
+      const res = await fetch('/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ usuario, password })

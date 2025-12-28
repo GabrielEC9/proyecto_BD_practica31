6445110053c191fa7744dae3095ad0b5ffd13482
GabrielEC9 <161523458+GabrielEC9@users.noreply.github.com>
2025-12-27 18:33:05 -0600

    Actualizacion ORM

--- public/auth.js
+++ public/auth.js
@@ -1,50 +1,7 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('login-form')
-  const errorMsg = document.getElementById('error-message')
-
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault()
-
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-
-    errorMsg.textContent = ''
-    errorMsg.classList.add('hidden')
-
-    try {
-      const response = await fetch('/api/login-orm', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ usuario: email, password })
-      })
-
-      const result = await response.json()
-
-      if (response.ok) {
-        console.log('Login exitoso:', result)
-        window.location.href = 'index.html' // redirige al inicio
-      } else {
-        errorMsg.textContent = result.error || 'Usuario o contraseña incorrectos'
-        errorMsg.classList.remove('hidden')
-      }
-
-    } catch (err) {
-      console.error('Error en fetch:', err)
-      errorMsg.textContent = 'Error de conexión con el servidor.'
-      errorMsg.classList.remove('hidden')
-    }
-  })
-})
-
-
-
-=======
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://lvuqrksujmgwgvebokgw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dXFya3N1am1nd2d2ZWJva2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxNzA0NzIsImV4cCI6MjA3NTc0NjQ3Mn0.-r4fp5yQi1pH2qHmbEbhm-6Q_4WgXc_yrr3JQZpGJV4'
-
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -59,10 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     errorMsg.textContent = ''
     errorMsg.classList.add('hidden')
 
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password })
 
     if (error) {
       console.error('Supabase error:', error)
@@ -75,6 +29,3 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 })
 
-
-
->>>>>>> 2e9ec9ec69639a0afd84dbdc1f2ac86bb4edaa46
--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'dotenv/config'
 import { createClient } from '@supabase/supabase-js'
 
@@ -6,11 +5,3 @@ export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
 )
-=======
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://lvuqrksujmgwgvebokgw.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
->>>>>>> 2e9ec9ec69639a0afd84dbdc1f2ac86bb4edaa46
